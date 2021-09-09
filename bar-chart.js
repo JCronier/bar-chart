548cecfd26603819509e0892a8ec1ef8020842f0
Jordan <jcronier777@gmail.com>
2021-09-09 23:43:03 +0000

    Chart outline and bar drawing

---
+++ bar-chart.js
@@ -0,0 +1,9 @@
+function generateBarChart() {
+	for (let i = 0; i < 6; i++) {
+  	$("#bar-chart").append("<div class=\"bar\"></div>");
+  }
+}
+
+function drawBarChart(data, options, element) {
+	
+}
